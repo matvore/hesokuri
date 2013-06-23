2de80424f4437a1867280a570a5357dc1212a5bb
Matthew DeVore <matvore@google.com>
2013-06-23 08:55:51 -0700

    Handle MAC addresses as pure fix-width hex values.

--- hesokuri.el
+++ hesokuri.el
@@ -22,20 +22,20 @@ hypothetical user with two Mac OS machines and a Linux machine:
 
 \((filing-cabinet
   (\"/Users/jdoe/filing-cabinet\"
-   3c:07:55:62:31:3f
-   12:f3:90:d5:1b:fd))
+   3c075562313f
+   12f390d51bfd))
  (emacs-d
   (\"/Users/jdoe/.emacs.d\"
-   3c:07:55:62:31:3f
-   12:f3:90:d5:1b:fd)
+   3c075562313f
+   12f390d51bfd)
   (\"/home/jdoe/.emacs.d\"
-   4c:e7:7b:30:23:3d))
+   4ce77b30233d))
  (hesokuri
   (\"/Users/matvore/hesokuri\"
-   3c:07:55:62:31:3f
-   12:f3:90:d5:1b:fd)
+   3c075562313f
+   12f390d51bfd)
   (\"/home/jdoe/hesokuri\"
-   4c:e7:7b:30:23:3d)))
+   4ce77b30233d)))
 
 Notice that the Mac OS machines use the same directory structure, so the
 directories only have to appear once in the list.
@@ -43,16 +43,20 @@ directories only have to appear once in the list.
 
 (defun hesokuri-mac-of (ip)
   "Given an IP address as a string, attempts to find the MAC address associated
-with it. The MAC address is returned as a symbol in the form ff:ff:ff:ff:ff:ff.
+with it. The MAC address is returned as a symbol in the form ffffffffffff.
 Returns NIL if there was an error."
   (with-temp-buffer
     (let ((arp-result (call-process "arp" nil t nil ip)))
       (unless (eql 0 arp-result)
         (error "Error running arp for ip address %s (exitcode %s): %s"
                ip arp-result (buffer-string)))
-      (let* ((arp-output (buffer-string))
+      (let* ((arp-output (downcase (buffer-string)))
              (mac-str-index
               (string-match "..?:..?:..?:..?:..?:..? " arp-output)))
         (unless mac-str-index
           (error "Could not find MAC addres in arp output: %s" arp-output))
-        (intern (substring arp-output mac-str-index (- (match-end 0) 1)))))))
+        (let* ((raw-mac (substring arp-output mac-str-index (1- (match-end 0))))
+               (two-digits-each
+                (mapcar (lambda (s) (if (eql 1 (length s)) (concat "0" s) s))
+                        (split-string raw-mac ":"))))
+          (intern (apply 'concat two-digits-each)))))))
