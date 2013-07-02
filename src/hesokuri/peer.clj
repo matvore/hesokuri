b333675a0c45164b950d6f657d1cd968bbc667a6
Matthew DeVore <matvore@google.com>
2013-07-02 13:22:56 -0700

    Simplify the push method on peer

--- src/hesokuri/peer.clj
+++ src/hesokuri/peer.clj
@@ -2,7 +2,16 @@
   "Implementation of the peer object. It optimizes access to a peer by keeping
   track of what has been successfully pushed to it and when it has last failed
   to respond to a ping. All intervals are in milliseconds unless otherwise
-  specified."
+  specified.
+  A peer object is a map with the following keys:
+  :last-fail-ping-time
+      The value returned by System/currentTimeMillis when the last test for
+      responsiveness failed.
+  :pushed
+      A map of keys in the form [local-path-to-source branch-name] to sha1 hash
+      strings, which indicate what was most recently pushed to the peer from
+      the given branch on the given source. If the current hash of the branch is
+      the same as the entry in the map, a push will not be attempted."
   (:use hesokuri.util)
   (:import (java.net InetAddress UnknownHostException)))
 
@@ -15,71 +24,50 @@
   10000)
 
 (defn new-peer
-  "Creates a new peer with default values for arguments and the given :host,
-  which is the hostname used to reach the peer."
-  [host]
-  {;; The value returned by System/currentTimeMillis when the last test for
-   ;; responsiveness failed.
-   :last-no-response-time 0
+  "A new peer with default values for each entry."
+  {:last-fail-ping-time 0 :pushed {}})
 
-   ;; A map of keys in the form [local-path-to-source branch-name] to sha1 hash
-   ;; strings, which indicate what was most recently pushed to the peer from
-   ;; the given branch on the given source. If the current hash of the branch is
-   ;; the same as the entry in the map, a push will not be attempted.
-   :pushed {}
-
-   ;; The hostname of the peer.
-   :host host})
+(defn reset-fail-ping-time
+  "Resets the :last-file-ping-time value so that the next push will always try
+  pinging again, which may time out for up to timeout-for-ping millis while
+  waiting for a response."
+  [self]
+  (assoc-in self [:last-file-ping-time 0]))
 
 (defn push
   "Performs a push. Branch name parameters can either be a string or something
   that evaluates to the name of the branch when str is invoked.
 
-  always-try - when truthy, indicates ignore the last-no-response-time so a
-      recent failed ping will not cause this function to no-op
   local-path - the path to the source to push from on the local machine
-  peer-path - the path to the source to push to on the peer (remote) machine
+  peer-repo - a hesokuri.util.PeerRepo object that indicates the peer and source
+      to push to.
   branch-name - the local name of the branch
   hash - the hash to push (in general, this should be the hash pointed to by
-      branch-name
-  normal-push-branches - branch names to attempt a non-forced pushed (tried
-      first)
-  force-push-branches - branch names to attempt a force push (-f). Tried only
-      if all the normal push branches failed."
-  [{:keys [last-no-response-time pushed host] :as self}
-   always-try
-   local-path peer-path
+      branch-name.
+  branches - a sequence of sequences in the form: [branch-name push-args].
+      'push-args' is a sequence of strings passed as arguments after 'git push'
+      on the command line. 'branch-name' is the destination branch name to use."
+  [{:keys [last-fail-ping-time pushed] :as self}
+   local-path peer-repo
    branch-name hash
-   normal-push-branches force-push-branches]
+   branches]
   (let [current-time (System/currentTimeMillis)
-        peer-repo (str (->PeerRepo host peer-path))
         pushed-key [local-path branch-name]]
     (cond
-     (or (and (not always-try) last-no-response-time
-              (< (- current-time last-no-response-time) minimum-retry-interval))
-         (= hash (pushed [local-path branch-name])))
+     (or (< (- current-time last-fail-ping-time) minimum-retry-interval)
+         (= hash (pushed pushed-key)))
      self
 
-     (try (-> host InetAddress/getByName (.isReachable timeout-for-ping))
-          (catch UnknownHostException _ false))
-     (assoc self :last-no-response-time current-time)
+     (try (-> (:host peer-repo) InetAddress/getByName
+              (.isReachable timeout-for-ping) not)
+          (catch UnknownHostException _ true))
+     (assoc self :last-fail-ping-time current-time)
 
      :else
-     (loop [normal-push-branches (seq normal-push-branches)
-            force-push-branches (seq force-push-branches)]
-       (cond
-        normal-push-branches
-        (if (= 0 (sh-print "git" "push" peer-repo
-                           (str hash ":" (first normal-push-branches))
-                           :dir local-path))
-          (assoc self :pushed (assoc pushed pushed-key hash))
-          (recur (next normal-push-branches) force-push-branches))
-
-        force-push-branches
-        (if (= 0 (sh-print "git" "push" "-f" peer-repo
-                           (str hash ":" (first force-push-branches))
-                           :dir local-path))
-          (assoc self :pushed (assoc pushed pushed-key hash))
-          (recur nil (next force-push-branches)))
-
-        :else self)))))
+     (-> (for [[branch-name & push-args] branches
+               :let [latter-args [peer-repo (str hash ":" branch-name)
+                                  :dir local-path]]
+               :when (= 0 (apply sh-print "git" "push"
+                                 `(~@push-args ~@latter-args)))]
+           (assoc-in self [:pushed pushed-key] hash))
+         first (or self)))))
