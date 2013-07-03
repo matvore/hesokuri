8bf089aaff43b57266ded790515e791aa9571a4f
Matthew DeVore <matvore@google.com>
2013-07-03 11:20:31 -0700

    Fix metadata in project file

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hesokuri "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "personal distributed backup and duplication system"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "TODO"
+            :url "http://go/kitteh"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main hesokuri.core)
