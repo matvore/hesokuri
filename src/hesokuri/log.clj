79edf16b6597e11ef8a01d1155523bc09f47899a
Matthew DeVore <matvore@google.com>
2013-07-05 14:05:40 -0700

    Document the log function

--- src/hesokuri/log.clj
+++ src/hesokuri/log.clj
@@ -9,4 +9,7 @@
   self)
 
 (defn log [fmt & args]
+  "Logs a formatted string, avoiding simultaneous logging by using an agent.
+  The arguments passed to this function are passed directly to 'format' to
+  format the string before logging it. A newline is appended automatically."
   (send logger -log (apply format fmt args)))
