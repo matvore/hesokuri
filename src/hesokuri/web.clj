927565bb6f38c7f6cdae8af4fd9961acf419fc19
Matthew DeVore <matvore@google.com>
2013-07-06 19:22:11 -0700

    Add CSS to make formatting just a little nicer

--- src/hesokuri/web.clj
+++ src/hesokuri/web.clj
@@ -1,11 +1,12 @@
 (ns hesokuri.web
   "Defines web pages that show and allow manipulation of hesokuri state."
   (:use hesokuri.core
-        [hiccup.page :only [html5]]
+        hiccup.page
         [noir.core :only [defpage]]))
 
 (defpage "/" []
   (html5
+   (include-css "/css.css")
    [:head [:title "heso main"]]
    [:body
     (let [heso @heso]
@@ -14,17 +15,17 @@
        [:h1 "more"]
        [:div [:a {:href "/sources"} "sources"]]
        [:h1 "config-file"]
-       [:div (heso :config-file)]
+       [:div#config-file (heso :config-file)]
        (for [source-index (-> :sources heso count range)
              :let [source ((heso :sources) source-index)]]
-         `[:div
+         `[:div#source-map
            ~@(concat (for [[host dir] source]
                        (format "%s %s<br>" host dir)))])])]))
 
 (defpage "/sources" []
   (html5
-   [:head
-    [:title "heso sources"]]
-   `[:body
-     ~@(for [[source-dir source-agent] (@heso :source-agents)]
-         [:h1 source-dir])]))
+   (include-css "/css.css")
+   [:head [:title "heso sources"]]
+     `[:body
+       ~@(for [[source-dir source-agent] (@heso :source-agents)]
+           [:h1 source-dir])]))
