e42f4b842fc0439e7bc40ddc0f171749d90adfd2
Matthew DeVore <matvore@google.com>
2013-07-07 09:24:39 -0700

    Refactor navbar rendering and show errors in separate page
    Put navbar rendering in a separate function called -navbar that shows all
    nav links but automatically makes the current page a non-link.
    
    Show full error information in a separate page, and make the error indicator
    on the sources page show right next to the source directory, rather than off
    to the side in a separate div.

--- src/hesokuri/web.clj
+++ src/hesokuri/web.clj
@@ -2,7 +2,29 @@
   "Defines web pages that show and allow manipulation of hesokuri state."
   (:use hesokuri.core
         hiccup.page
-        [noir.core :only [defpage defpartial]]))
+        [hiccup.util :only [escape-html]]
+        [noir.core :only [defpage defpartial]]
+        [ring.util.codec :only [url-decode url-encode]]))
+
+(defpartial -navbar [heso & [url]]
+  (let [link (fn [link-url title]
+               (cond
+                (= url link-url)
+                [:div#nav-el title]
+                :else
+                [:div#nav-el [:a {:href link-url} title]]))]
+    [:div#nav
+     (link "/" "config")
+     (link "/peers" "peers")
+     (link "/sources" "sources")
+     [:div#nav-el "local-identity: " (heso :local-identity)]]))
+
+(defpartial -errors [detail-link errs]
+  (html5
+   (cond
+    (-> errs count zero?) [:div#no-errors "no errors"]
+    :else
+    [:div#errors [:a#error-link {:href detail-link} "errors!"]])))
 
 (defpage "/" []
   (html5
@@ -11,9 +33,7 @@
    [:body
     (let [heso (heso-snapshot @heso)]
       [:div
-       [:div#nav-el [:a {:href "/peers"} "peers"]]
-       [:div#nav-el [:a {:href "/sources"} "sources"]]
-       [:div#nav-el "local-identity: " (heso :local-identity)]
+       (-navbar heso "/")
        [:h1 "config-file"]
        [:div#config-file (heso :config-file)]
        (for [source-index (-> :sources heso count range)
@@ -22,12 +42,27 @@
            (for [[host dir] source]
              (format "%s %s<br>" host dir))])])]))
 
-(defpartial -errors [errs]
-  (html5
-   (cond
-    (-> errs count zero?) [:div#no-errors-wrapper "no errors"]
-    :else
-    [:div#errors-wrapper "errors!"])))
+(defpage "/errors/:type/:key" {:keys [type key]}
+  (let [heso (heso-snapshot @heso)
+        errors (get-in heso [(keyword type) key :errors])]
+    (html5
+     (include-css "/css.css")
+     [:head [:title (str "errors for " type " " key)]]
+     [:body
+      (-navbar heso "")
+      [:form {:id "clear-form", :action "/errors/clear", :method "post"}
+       [:input {:type "text", :name "type", :value type, :hidden "true"}]
+       [:input {:type "text", :name "key", :value key, :hidden "true"}]
+       [:a {:href "javascript: document.forms['clear-form'].submit()"} "clear"]]
+      [:pre
+       (for [error errors
+               :let [err-string-writer (java.io.StringWriter.)
+                     string-printer (java.io.PrintWriter. err-string-writer)]]
+         (do
+           (.printStackTrace error string-printer)
+           (.println string-printer (apply str (repeat 80 "-")))
+           (.flush string-printer)
+           [:div#stack-trace (escape-html (.toString err-string-writer))]))]])))
 
 (defpage "/sources" []
   (html5
@@ -35,13 +70,12 @@
    [:head [:title "heso sources"]]
    (let [heso (heso-snapshot @heso)]
      [:body
-      [:div#nav-el [:a {:href "/"} "config"]]
-      [:div#nav-el [:a {:href "/peers"} "peers"]]
-      [:div#nav-el "local-identity: " (heso :local-identity)]
+      (-navbar heso "/sources")
       (for [[source-dir source] (heso :source-info)]
-        [:div
         [:div#source-info-wrapper
-         [:h1 source-dir]
+         [:div#source-heading source-dir]
+         (-errors (str "/errors/source-info/" (url-encode source-dir))
+                  (source :errors))
          (for [[branch hash] (source :branches)]
            [:div#branch-info
             [:div#branch-heading
@@ -52,8 +86,7 @@
                    :let [pushed ((peer :pushed) [source-dir branch])]]
                [:tr
                 [:td peer-host]
-                [:td (if (= hash pushed) "ok" (or pushed "none"))]])]])]
-         (-errors (source :errors))])])))
+                [:td (if (= hash pushed) "ok" (or pushed "none"))]])]])])])))
 
 (defpage "/peers" []
   (html5
@@ -61,6 +94,4 @@
    [:head [:title "heso peers"]]
    (let [heso (heso-snapshot @heso)]
      [:body
-      [:div#nav-el [:a {:href "/"} "config"]]
-      [:div#nav-el [:a {:href "/sources"} "sources"]]
-      [:div#nav-el "local-identity: " (heso :local-identity)]])))
+      (-navbar heso "/peers")])))
