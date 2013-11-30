25eae25d399cabb32ee82aed16993a8627e435f3
Matthew DeVore <matvore@google.com>
2013-11-30 08:40:47 -0800

    Remove support for the polling Barbary watch service
    We don't use it and don't want to have to maintain it.

--- third_party/barbarywatchservice/src/com/barbarysoftware/watchservice/WatchService.java
+++ third_party/barbarywatchservice/src/com/barbarysoftware/watchservice/WatchService.java
@@ -66,8 +66,8 @@ public abstract class WatchService implements Closeable {
         final String osVersion = System.getProperty("os.version");
         final int minorVersion = Integer.parseInt(osVersion.split("\\.")[1]);
         if (minorVersion < 5) {
-            // for Mac OS X prior to Leopard (10.5)
-            return new MacOSXPollingWatchService();
+            throw new UnsupportedOperationException("barbarywatchservice not "
+                    + "supported on Mac OS X prior to Leopard (10.5)");
             
         } else {
             // for Mac OS X Leopard (10.5) and upwards
