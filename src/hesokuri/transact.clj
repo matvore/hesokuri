0a63bc7224c1b5df81e50727ca1cd26341919654
Matthew DeVore <matvore@google.com>
2014-05-02 23:55:16 -0700

    Add hesokuri.transact namespace
    This will let us keep track of errors when writing and reading to a Git
    repository.

---
+++ src/hesokuri/transact.clj
@@ -0,0 +1,94 @@
+; Copyright (C) 2014 Google Inc.
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;    http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(ns hesokuri.transact
+  "This namespace models transactions, which are used to gracefully handle
+problems that may occur when performing operations on a database (e.g. Git
+repo). Operations are assumed to consist of read and write operations which may
+occur concurrently, and which must be cleaned up using the Closeable interface.
+A transaction keeps track of all Exceptions that occur and all Closeables that
+must be closed. Upon normal termination of a transaction, the current thread
+blocks until all Closeables have been closed, and the result of the transaction
+function and a vector of all Exceptions are returned. If the transaction
+function throws an Exception (as opposed to an Exception happening during a
+concurrent operation in the transaction), the Exception is added to the end of
+the vector of Exceptions, the transaction ends, and all Closeables are closed
+immediately.
+
+A transaction is actually an atom which contains information about the opened
+Closeables and Exceptions that have occurred. For information on implementing a
+function that supports transactions, (i.e. receives a transaction as an argument
+and uses it to report events), see the open, close, and error functions in this
+namespace. These functions should all be called with swap!. For information on
+consuming an API that uses transactions, see the transact function.")
+
+(defn open
+  "Adds a closeable to the list of not-yet-closed items."
+  [{:keys [opened] :as trans} closeable]
+  (assoc trans :opened (conj opened closeable)))
+
+(defn close
+  "Closes the given closeable in the given transaction. This function will
+call the close method on Closeable itself."
+  [{:keys [opened errors-promise errors] :as trans} closeable]
+  (.close closeable)
+  (let [opened (disj opened closeable)]
+    (when (and errors-promise (empty? opened))
+      (deliver errors-promise errors))
+    (assoc trans :opened opened)))
+
+(defn finish
+  "Considers the transaction finished. Will deliver the promise specified by
+errors-promise once all Closeables have been closed, or immediately if they are
+already closed. This will also close the remaining ones immediately if exception
+is truthy. (The exception argument indicates what caused the trans-fn to
+terminate abruptly, if applicable.)"
+  [{:keys [opened errors] :as trans} errors-promise exception]
+  (cond
+   exception (do (doseq [closeable opened]
+                   (.close closeable))
+                 (deliver errors-promise (conj errors exception))
+                 nil)
+   (empty? opened) (do (deliver errors-promise errors)
+                       nil)
+   :else (assoc trans :errors-promise errors-promise)))
+
+(defn error
+  "Reports an error to the given transaction. The error should be a Throwable."
+  [{:keys [errors-promise errors] :as trans} error]
+  (if (and errors-promise (realized? errors-promise))
+    (throw error)
+    (assoc trans :errors (conj errors error))))
+
+(defn transact
+  "Runs the given function in a transaction. trans-fn is called with a single
+argument: the transaction itself, which should be passed to all functions that
+open/close streams and report errors.
+
+This function will not return until all Closeables opened during the transaction
+have been closed, so if you forget to close any (like not reading until the end
+of a sequence backed by a Closeable stream), this function will block forever.
+
+This function returns a sequence with at least two items: the value returned by
+trans-fn (nil if trans-fn threw an Exception) and a sequence of errors that
+occurred, in the order that they were reported. The caller should not commit
+any changes, or overwrite or destroy existing data, if any errors occurred."
+  [trans-fn]
+  (let [trans (atom {:opened #{}, :errors []})]
+    (let [[res exception] (try [(trans-fn trans) nil]
+                               (catch Throwable e [nil e]))
+          errors-promise (promise)
+          errors (do (swap! trans finish errors-promise exception)
+                     @errors-promise)]
+      [res errors])))
---
+++ test/hesokuri/transact_test.clj
@@ -0,0 +1,72 @@
+; Copyright (C) 2014 Google Inc.
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;    http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(ns hesokuri.transact-test
+  (:use clojure.test
+        hesokuri.transact))
+
+(deftest test-noop-transaction
+  (is (= [42 []] (transact (fn [_] 42)))))
+
+(deftest test-body-throws-exception
+  (let [e (Exception.)]
+    (is (= [nil [e]] (transact (fn [_] (throw e)))))))
+
+(deftest test-has-error-and-terminates-normally
+  (let [e (Exception.)]
+    (is (= [42 [e]]
+           (transact (fn [trans]
+                       (swap! trans error e)
+                       42))))))
+
+(deftest test-has-error-and-has-open-and-throws-exception
+  (let [closed (atom #{})
+        closeable (fn [i] (reify
+                            java.io.Closeable
+                            (close [_] (swap! closed #(conj % i)))))
+        opened-1 (closeable 1)
+        opened-2 (closeable 2)
+        error-1 (Exception. "1")
+        error-2 (Exception. "2")
+        res (transact
+             (fn [trans]
+               (swap! trans error error-1)
+               (swap! trans open opened-1)
+               (swap! trans open opened-2)
+               (swap! trans close opened-1)
+               (throw error-2)))]
+    (is (= [nil [error-1 error-2]] res))
+    (is (= #{1 2} @closed))))
+
+(deftest test-close-after-finish
+  (let [some-error (Exception. "error")
+        opened (java.io.ByteArrayOutputStream.)
+        results (fn [e]
+                  (let [errors-promise (promise)
+                        trans (-> {:opened #{}, :errors []}
+                                  (open opened)
+                                  ((if e #(error % e) identity))
+                                  (finish errors-promise nil))]
+                    [(boolean (realized? errors-promise))
+                     (count (:opened trans))
+                     (dissoc (close trans opened) :errors-promise)
+                     @errors-promise]))]
+    (are [e]
+      (= [false
+          1
+          {:opened #{}, :errors (if e [e] [])}
+          (if e [e] [])]
+         (results e))
+      some-error
+      nil)))
