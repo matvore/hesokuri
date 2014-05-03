d3397cf13ec81686e53c9e0ff47b75f5ac778e39
Matthew DeVore <matvore@google.com>
2014-05-03 02:13:59 -0700

    Reimplement Git functions in hesokuri.repl using transact utility

--- repl.clj
+++ repl.clj
@@ -18,8 +18,10 @@
 ;;;; thoroughly unit tested.
 
 (require '[clojure.java.io :as cjio])
+(require '[clojure.string :as cstr])
 (require '[clojure.tools.logging :as ctl])
 (require '[hesokuri.git :as git])
+(require '[hesokuri.transact :as transact])
 
 (use 'clojure.repl)
 
@@ -29,18 +31,39 @@ sequence of tree entries similar to those returned by read-tree. However, each
 (sub)tree entry (the entry itself being a sequence) has at least one extra item:
 the result of read-tree* for that subtree. The value is in a lazy-seq, so it
 will not be read from the repository until it is accessed."
-  [git git-dir tree-hash]
-  (let [git-args (git/args git-dir ["cat-file" "tree" (str tree-hash)])
-        [_ stdout :as cat-tree] (git/invoke-streams git git-args)]
-    (concat
-     (for [[type _ hash :as info] (git/read-tree stdout)]
-       (concat info
-               (lazy-seq
-                (when (= type "40000")
-                  [(read-tree* git git-dir hash)]))))
-     (lazy-seq (.close stdout)
-               (git/if-error cat-tree #(ctl/error %))
-               nil))))
+  ([git git-dir tree-hash] (read-tree* git git-dir tree-hash (atom nil)))
+  ([git git-dir tree-hash trans]
+     (let [git-args (git/args git-dir ["cat-file" "tree" (str tree-hash)])
+           [_ stdout :as cat-tree] (git/invoke-streams git git-args)]
+       (swap! trans transact/open stdout)
+       (concat
+        (for [[type _ hash :as info] (git/read-tree stdout)]
+          (concat info
+                  (lazy-seq
+                   (when (= type "40000")
+                     [(read-tree* git git-dir hash trans)]))))
+        (lazy-seq (swap! trans transact/close stdout)
+                  (git/if-error cat-tree #(swap! trans transact/error
+                                                 (java.io.IOException. %)))
+                  nil)))))
+
+(defn write-blob
+  "Writes a blob to the given Git repository. in is an instance of
+clojure.java.io/IOFactory which is the blob data to be written."
+  ([git git-dir in] (write-blob git git-dir in (atom nil)))
+  ([git git-dir in trans]
+     (let [cat-blob-args (git/args git-dir ["hash-object" "-w" "--stdin"])
+           [blob-in blob-out :as cat-blob]
+           (git/invoke-streams git cat-blob-args)]
+       (try
+         (transact/with-closeables trans
+           [blob-in blob-out]
+           (fn []
+             (cjio/copy in blob-in)
+             (.close blob-in)
+             (cstr/trim (slurp blob-out))))
+         (finally (git/if-error cat-blob #(swap! trans transact/error
+                                                 (java.io.IOException. %))))))))
 
 (defn write-tree*
   "Writes a tree into a Git repository. The tree is a structure similar to that
@@ -50,31 +73,33 @@ clojure.java.io/IOFactory instance is after the nil value. For each tree that
 must be updated, a the original tree structure (usually after the hash) has been
 replaced with a different one of the same format. This function returns the Hash
 of the tree that was written."
-  [git git-dir tree]
-  (let [cat-tree-args (git/args git-dir ["hash-object" "-w" "--stdin" "-t"
-                                         "tree"])
-        [stdin stdout :as cat-tree] (git/invoke-streams git cat-tree-args)]
-    (try
-      (doseq [[type name hash replace] tree]
-        (let [new-hash
-              (cond
-               hash hash
-               (= type "40000") (write-tree* git git-dir replace)
-               :else (let [cat-blob-args
-                           (git/args git-dir ["hash-object" "-w" "--stdin"])
-                           [blob-in blob-out :as cat-blob]
-                           (git/invoke-streams git cat-blob-args)]
-                       (try
-                         (clojure.java.io/copy
-                          (clojure.java.io/input-stream replace) blob-in)
-                         (.close blob-in)
-                         (slurp blob-out)
-                         (finally (.close blob-in)
-                                  (.close blob-out)
-                                  (git/if-error cat-blob #(ctl/error %))))))]
-          (git/write-tree-entry stdin [type name new-hash])))
-      (.close stdin)
-      (slurp stdout)
-      (finally (.close stdin)
-               (.close stdout)
-               (git/if-error cat-tree #(ctl/error %))))))
+  ([git git-dir tree] (write-tree* git git-dir tree (atom nil)))
+  ([git git-dir tree trans]
+     (let [cat-tree-args (git/args git-dir ["hash-object" "-w" "--stdin" "-t"
+                                            "tree"])
+           [stdin stdout :as cat-tree] (git/invoke-streams git cat-tree-args)]
+       (try
+         (transact/with-closeables trans
+           [stdin stdout]
+           (fn []
+             (doseq [[type name hash replace] tree]
+               (let [new-hash
+                     (cond
+                      hash hash
+                      (= type "40000") (write-tree* git git-dir replace trans)
+                      :else (write-blob git git-dir replace trans))]
+                 (git/write-tree-entry stdin [type name new-hash])))
+             (.close stdin)
+             (cstr/trim (slurp stdout))))
+         (finally (git/if-error cat-tree #(swap! trans transact/error
+                                                 (java.io.IOException. %))))))))
+
+(comment
+  ;; Recursively read a tree at the given hash
+  (let [hash "a9ef38249198b290668ebfb2e29ca5d528a88661"]
+    (pprint (read-tree* "git" "/Users/matvore/hesokuri/.git" hash)))
+
+  (write-tree* "git" "/Users/matvore/hesokuri/.git"
+               [["100644" "foo" nil (.getBytes "at top dir\n")]
+                ["40000" "bar" nil [["100644" "file" nil
+                                     (.getBytes "in dir\n")]]]]))
