2f2c15277e6d14f9825c32b7591d94a974e86a19
Matthew DeVore <matvore@google.com>
2014-05-03 19:00:34 -0700

    Remove functionality from hesokuri.transact
    This functionality probably will never be needed, and it made the purpose of the
    namespace unclear.

--- src/hesokuri/transact.clj
+++ src/hesokuri/transact.clj
@@ -13,105 +13,42 @@
 ; limitations under the License.
 
 (ns hesokuri.transact
-  "This namespace models transactions, which are used to gracefully handle
-problems that may occur when performing operations on a database (e.g. Git
-repo). Operations are assumed to consist of read and write operations which may
-occur concurrently, and which must be cleaned up using the Closeable interface.
-A transaction keeps track of all Exceptions that occur and all Closeables that
-must be closed. Upon normal termination of a transaction, the current thread
-blocks until all Closeables have been closed, and the result of the transaction
-function and a vector of all Exceptions are returned. If the transaction
-function throws an Exception (as opposed to an Exception happening during a
-concurrent operation in the transaction), the Exception is added to the end of
-the vector of Exceptions, the transaction ends, and all Closeables are closed
-immediately.
+  "This namespace supplies transactions functionality, which solves the problem
+of lazy sequences, backed by closeable resources, that are not iterated over
+completely before being garbage collected, which may be a result of an
+Exception, or buggy code iterating over it.
 
-A transaction is actually an atom which contains information about the opened
-Closeables and Exceptions that have occurred. For information on implementing a
-function that supports transactions, (i.e. receives a transaction as an argument
-and uses it to report events), see the open, close, and error functions in this
-namespace. These functions should all be called with swap!. For information on
-consuming an API that uses transactions, see the transact function.
+To use such sequences, you only need to understand the transact function. To
+implement such sequences, you should understand the open and close functions.
+open should be called to report that a new stream has been opened. close is to
+report that a sequence has finished with a stream in a normal fashion, and it
+should be closed.
 
-If any events are sent to the transaction after it is finished (i.e. after
-transact has returned), then the events will behave very simply: open and close
-events are ignored, but Closeables will still actually be closed, and errors are
-thrown with (throw). This is also what happens if you use a transaction that is
-(atom nil), which you can do if you are in the REPL. This is more flexible. If
-you accidentally read using the transaction after transact returns, you may not
-notice, but this is easy to avoid: simply don't return anything from transact
-except for metadata about your completed commit.")
+Any stream that is not closed with the close function before the transaction is
+over will be closed immediately when the transaction is over. This will cause
+problems if streams are still being used in other threads, so if you use these
+lazy sequences, you should perform some synchronization in your transaction to
+wait for other transactions to finish.")
 
-(defn open
+(def open
   "Adds a closeable to the list of not-yet-closed items."
-  [{:keys [opened] :as trans} closeable]
-  (when trans (assoc trans :opened (conj opened closeable))))
+  conj)
 
 (defn close
   "Closes the given closeable in the given transaction. This function will
 call the close method on Closeable itself."
-  [{:keys [opened errors-promise errors] :as trans} closeable]
+  [opened closeable]
   (.close closeable)
-  (when trans
-    (let [opened (disj opened closeable)]
-      (if (and errors-promise (empty? opened))
-        (do (deliver errors-promise errors)
-            nil)
-        (assoc trans :opened opened)))))
-
-(defn with-closeables
-  "Runs the given function f with some Closeables open in the given transaction.
-This automatically invokes open and close on the transaction before and after
-invoking the function. The return value of with-closeables is the return of f.
-Note this function should not be called with swap!."
-  [trans-atom closeables f]
-  (swap! trans-atom (partial reduce #(open %1 %2)) closeables)
-  (try (f)
-       (finally
-         (swap! trans-atom (partial reduce #(close %1 %2)) closeables))))
-
-(defn finish
-  "Considers the transaction finished. Will deliver the promise specified by
-errors-promise once all Closeables have been closed, or immediately if they are
-already closed. This will also close the remaining ones immediately if exception
-is truthy. (The exception argument indicates what caused the trans-fn to
-terminate abruptly, if applicable.)"
-  [{:keys [opened errors] :as trans} errors-promise exception]
-  {:pre [trans]}
-  (cond
-   exception (do (doseq [closeable opened]
-                   (.close closeable))
-                 (deliver errors-promise (conj errors exception))
-                 nil)
-   (empty? opened) (do (deliver errors-promise errors)
-                       nil)
-   :else (assoc trans :errors-promise errors-promise)))
-
-(defn error
-  "Reports an error to the given transaction. The error should be a Throwable."
-  [{:keys [errors-promise errors] :as trans} error]
-  (if (not trans)
-    (throw error)
-    (assoc trans :errors (conj errors error))))
+  (disj opened closeable))
 
 (defn transact
   "Runs the given function in a transaction. trans-fn is called with a single
 argument: the transaction itself, which should be passed to all functions that
-open/close streams and report errors.
+open and close streams in an asynchronous or lazy manner.
 
-This function will not return until all Closeables opened during the transaction
-have been closed, so if you forget to close any (like not reading until the end
-of a sequence backed by a Closeable stream), this function will block forever.
-
-This function returns a sequence with at least two items: the value returned by
-trans-fn (nil if trans-fn threw an Exception) and a sequence of errors that
-occurred, in the order that they were reported. The caller should not commit
-any changes, or overwrite or destroy existing data, if any errors occurred."
+This function returns whatever trans-fn returns, or throws whatever trans-fn
+throws."
   [trans-fn]
-  (let [trans (atom {:opened #{}, :errors []})]
-    (let [[res exception] (try [(trans-fn trans) nil]
-                               (catch Throwable e [nil e]))
-          errors-promise (promise)
-          errors (do (swap! trans finish errors-promise exception)
-                     @errors-promise)]
-      [res errors])))
+  (let [trans (atom #{})]
+    (try (trans-fn trans)
+         (finally (doseq [c @trans] (.close c))))))
--- test/hesokuri/transact_test.clj
+++ test/hesokuri/transact_test.clj
@@ -17,86 +17,33 @@
         hesokuri.transact))
 
 (deftest test-noop-transaction
-  (is (= [42 []] (transact (fn [_] 42)))))
+  (is (= 42 (transact (fn [_] 42)))))
 
 (deftest test-body-throws-exception
-  (let [e (Exception.)]
-    (is (= [nil [e]] (transact (fn [_] (throw e)))))))
-
-(deftest test-has-error-and-terminates-normally
-  (let [e (Exception.)]
-    (is (= [42 [e]]
-           (transact (fn [trans]
-                       (swap! trans error e)
-                       42))))))
+  (is (thrown? Exception (transact (fn [_] (throw (Exception.)))))))
 
 (defn closeable [closed-atom index]
   (reify
     java.io.Closeable
     (close [_] (swap! closed-atom #(conj % index)))))
 
-(deftest test-has-error-and-has-open-and-throws-exception
+(deftest test-transact-closes-pending-upon-completion
   (let [closed (atom #{})
         opened-1 (closeable closed 1)
         opened-2 (closeable closed 2)
-        error-1 (Exception. "1")
-        error-2 (Exception. "2")
-        res (transact
-             (fn [trans]
-               (swap! trans error error-1)
-               (swap! trans open opened-1)
-               (swap! trans open opened-2)
-               (swap! trans close opened-1)
-               (throw error-2)))]
-    (is (= [nil [error-1 error-2]] res))
-    (is (= #{1 2} @closed))))
-
-(deftest test-with-closeables-closes-after-invoke
-  (let [closed (atom #{})
-        opened-1 (closeable closed 1)
-        opened-2 (closeable closed 2)
-        res (transact
-             (fn [trans]
-               (with-closeables trans [opened-1 opened-2]
-                 (fn [] [42 (:opened @trans)]))))]
-    (is (= #{1 2} @closed))
-    (is (= [[42 #{opened-1 opened-2}] []] res))))
-
-(deftest test-close-after-finish
-  (let [some-error (Exception. "error")
-        opened (java.io.ByteArrayOutputStream.)
-        results (fn [e]
-                  (let [errors-promise (promise)
-                        trans (-> {:opened #{}, :errors []}
-                                  (open opened)
-                                  ((if e #(error % e) identity))
-                                  (finish errors-promise nil))]
-                    [(boolean (realized? errors-promise))
-                     (count (:opened trans))
-                     (close trans opened)
-                     @errors-promise]))]
-    (are [e]
-      (= [false
-          1
-          nil
-          (if e [e] [])]
-         (results e))
-      some-error
-      nil)))
-
-(deftest test-open-for-nil-trans
-  (is (nil? (open nil "foo"))))
-
-(deftest test-close-for-nil-trans
-  (let [closed (atom #{})
-        opened-1 (closeable closed 1)]
-    (is (nil? (close nil opened-1)))
-    (is (= #{1} @closed))))
-
-(deftest test-error-throws-for-nil-trans
-  (try
-    (error nil (Exception. "expected"))
-    (throw (Exception. "should have thrown"))
-    (catch Exception e
-      (is (= "expected" (.getMessage e))))))
+        make-trans-fn
+        (fn [lastly]
+          (fn [trans]
+            (swap! trans open opened-1)
+            (swap! trans open opened-2)
+            (lastly)))]
+    (are [lastly return]
+      (let [do-trans #(transact (make-trans-fn lastly))]
+        (swap! closed (constantly #{}))
+        (if return
+          (is (= return (do-trans)))
+          (is (thrown? Exception (do-trans))))
+        (is (= #{1 2} @closed)))
+      #(throw (Exception.)) nil
+      (constantly 42) 42)))
 
