c11a5be6cec62a349722a5cbddb3823c1dd68521
Matthew DeVore <matvore@google.com>
2014-05-16 21:11:04 -0700

    Prepare hesokuri.main to implement some extra commands
    This is also a clean-up, in order to maintain consistency between new and old
    code.
    
    - Functions that calculate a value based on environment variables and system
      properties have been changed to plain variables that are evaluated once at
      start-up. This prevents code from having to bind the function to a local
      variable when using it. This also means config-file is no longer testable, but
      making config-file a function made the -main function more complicated, and
      that function is NOT tested, so overall it was harming stability.
    - Add a usage message.
    - Rename port to web-port to avoid confusion.
    - Used the Java system property user.home rather than the environment variable
      HOME. The latter generally does not have a value on Windows.
    
    Symbols in this namespace are not, and should not be referenced by other
    namespaces, so refactoring like this is not risky. Tried all new and existing
    code paths to manually confirm functionality.

--- src/hesokuri/main.clj
+++ src/hesokuri/main.clj
@@ -16,34 +16,95 @@
   (:use hesokuri.util
         [ring.adapter.jetty :only [run-jetty]]
         [ring.middleware.params :only [wrap-params]])
-  (:require [hesokuri.dynamic-config :as dynamic-config]
+  (:require [clojure.java.io :as cjio]
+            [hesokuri.dynamic-config :as dynamic-config]
             [hesokuri.heso :as heso]
             [hesokuri.web :as web])
   (:gen-class))
 
-(defn- port []
-  "Returns the port to serve the heso web UI."
+(def web-port
+  "The port to serve the heso web UI."
   (Integer. (or (getenv "HESOPORT") "8080")))
 
-(defn- config-file []
-  (or (getenv "HESOCFG")
-      (str (getenv "HOME") "/.hesocfg")))
+(def home
+  "The user's home directory. This is used as a default for some settings."
+  (cjio/file (System/getProperty "user.home")))
+
+(def config-file
+  (cjio/file (or (getenv "HESOCFG")
+                 (cjio/file home ".hesocfg"))))
+
+(def hesobase-git-dir
+  "Location of the hesobase .git directory."
+  (cjio/file (or (getenv "HESOROOT") home)
+             ".hesobase.git"))
+
+(def default-proto-port
+  "The default protocol port. This number is pronounced 'hesoku,' because 8
+sounds like 'hay,' 50 looks like 'so', and 9 in Japanese is 'ku.'"
+  8509)
+
+(def usage
+  (str "Hesokuri: The distributed Git repo sync daemon
+Copyright (C) 2014 Google Inc.
+
+USAGE
+-----
+lein run
+  Starts Hesokuri process.
+
+lein run help
+  Shows this help.
+
+Experimental features
+---------------------
+lein run init MACHINE-NAME [PORT]
+  MACHINE-NAME is the name (and address) of this machine on the Hesokuri
+  network. PORT is the local port to use for communicating with other machines
+  on the network, or " default-proto-port " if omitted.
+"))
+
+(defn exit
+  "Prints some text to the given stream and exits with the given exit code."
+  [message out code]
+  (cjio/copy message out)
+  (.flush out)
+  (System/exit code))
 
 (defn -main
-  "Starts up hesokuri."
+  "Starts up Hesokuri or performs some administration task."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (let [config-file (config-file)
-        heso-agent (agent (heso/with-config []))
+  (cond
+   (empty? args)
+    (let [heso-agent (agent (heso/with-config []))
 
-        dynamic-config-agent
-        (->> (cb [heso-agent] [config]
-                 (send heso-agent heso/update-config config))
-             (dynamic-config/of config-file)
-             agent)]
-    (send dynamic-config-agent dynamic-config/start)
-    (alter-var-root #'hesokuri.web/*web-heso* (constantly heso-agent)))
-  (run-jetty (-> web/heso-web-routes
-                 wrap-params)
-             {:port (port) :join? false}))
+          dynamic-config-agent
+          (->> (cb [heso-agent] [config]
+                   (send heso-agent heso/update-config config))
+               (dynamic-config/of config-file)
+               agent)]
+      (send dynamic-config-agent dynamic-config/start)
+      (alter-var-root #'hesokuri.web/*web-heso* (constantly heso-agent))
+      (run-jetty (-> web/heso-web-routes
+                     wrap-params)
+                 {:port web-port :join? false}))
+   (= "init" (first args))
+    (let [[_ machine-name proto-port & extra-args] args
+          proto-port (if proto-port
+                       (try (Integer. proto-port)
+                            (catch NumberFormatException _
+                              (exit
+                               (format "Invalid port number: '%s'\n" proto-port)
+                               *err* 1)))
+                       default-proto-port)]
+      (when (or (seq extra-args)
+                (not machine-name))
+        (exit usage *err* 1))
+      ;; TODO: actually do initialization logic here. We should create a new
+      ;; repository for the hesobase and a key pair for this machine's
+      ;; identity.
+      )
+   (= ["help"] args) (exit usage *out* 0)
+   :else (exit usage *err* 1)))
